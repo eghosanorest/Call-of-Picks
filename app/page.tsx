26e3e83a00059fcbaeadab9173083565a4604c85
Dein GitHub Name <deine-github-mail@example.com>
2026-03-25 00:35:40 +0100

    add super rarity to admin item uploader

--- app/page.tsx
+++ app/page.tsx
@@ -5593,7 +5593,7 @@ setChatList([]);
     loop
     muted
     playsInline
-    className="absolute left-1/2 top-1/2 h-[300%] w-full -translate-x-1/2 -translate-y-1/2 object-cover"
+    className="absolute left-1/2 top-1/2 h-[600%] w-[120%] -translate-x-1/2 -translate-y-1/2 object-cover"
   />
 
   {/* ✨ optionaler Glow darüber */}
